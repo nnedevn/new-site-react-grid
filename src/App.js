67baf0c29b402591555c6486657695479acfa8fa
Nikolay Nedev <nedevn@gmail.com>
2018-06-26 15:27:20 -0700

    Adds react-router and Creates basic routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Link,
   Switch,
-  Redirect,
-} from "react-router";
+  Redirect
+} from "react-router-dom";
 
 // Import Components
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Projects from "./Components/Projects";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router className="App-intro">
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav role="nav">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </nav>
+
+        <div>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
       </div>
-    );
-  }
-}
+    </Router>
+  );
+};
 
 export default App;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
-<h1>Contact</h1>
+
+const Contact = () => {
+  return(
+    <div>
+      <h1>Contact</h1>
+    </div>
+  )
+}
+
+export default Contact;
+
