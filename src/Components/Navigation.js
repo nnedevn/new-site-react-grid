4114abe85289e9b2048c904971ab4ea13d62983a
Nikolay Nedev <nedevn@gmail.com>
2018-07-03 14:57:31 -0700

    Adds a toggle function

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => {
-  return (
-    <nav role="nav">
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/projects">Projects</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </nav>
-  );
+class Navigation extends Component {
+  constructor(){
+    super();
+    this.state = {
+      menuToggleActive: false,
+    }
+  }
+
+  burgerToggle = () => {
+    this.setState({menuToggleActive: !this.state.menuToggleActive})
+  }
+
+  render(){
+
+
+
+    return (
+      <nav crole="nav">
+        <ul className="wide-nav">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+  
+        <ul className="narrow-nav" className={this.state.menuToggleActive ? 'displayToggle' : null}>
+          <a onClick={this.burgerToggle}><i className="fa fa-bars fa-2x"></i></a>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+  
 };
 
 export default Navigation;
