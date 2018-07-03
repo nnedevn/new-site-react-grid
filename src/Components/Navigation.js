5c51f8b447d82e68c1f4af1530c1bbb9ddd88cd2
Nikolay Nedev <nedevn@gmail.com>
2018-07-03 15:08:11 -0700

    Adds nav responsive functionality

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,21 +2,18 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Navigation extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      menuToggleActive: false,
-    }
+      menuToggleActive: false
+    };
   }
 
   burgerToggle = () => {
-    this.setState({menuToggleActive: !this.state.menuToggleActive})
-  }
+    this.setState({ menuToggleActive: !this.state.menuToggleActive });
+  };
 
-  render(){
-
-
-
+  render() {
     return (
       <nav crole="nav">
         <ul className="wide-nav">
@@ -30,23 +27,31 @@ class Navigation extends Component {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-  
-        <ul className="narrow-nav" className={this.state.menuToggleActive ? 'displayToggle' : null}>
-          <a onClick={this.burgerToggle}><i className="fa fa-bars fa-2x"></i></a>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+
+        <ul className="narrow-nav">
+          <a onClick={this.burgerToggle}>
+            <i className="fa fa-bars fa-2x" />
+          </a>
+          <div
+            className="narrow-links"
+            className={
+              this.state.menuToggleActive ? "displayMobileNav" : "hideMobileNav"
+            }
+          >
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </div>
         </ul>
       </nav>
     );
   }
-  
-};
+}
 
 export default Navigation;
