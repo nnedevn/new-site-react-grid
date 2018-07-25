8bcf29998df2444ca54744e7f57ffee3a807aecd
Nikolay Nedev <nedevn@gmail.com>
2018-07-25 16:57:24 -0700

    Adds Project Buttons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects</h1>
-      
+
       <div className="project">
         <img src="http://via.placeholder.com/200x200" alt="" />
         <ul class="devicons">
@@ -21,12 +21,15 @@ const Projects = () => {
         </ul>
         <h2>This is the project title</h2>
         <div className="buttons">
-          <button>View on GitHub</button>
+          <button className="fill">View on GitHub</button>
           <button>Launch Project</button>
         </div>
-        <p>This is a short description of the projet Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nobis velit non voluptatem a autem expedita quasi eos iusto ratione, ducimus excepturi voluptas eligendi numquam.</p>
-        
-        
+        <p>
+          This is a short description of the projet Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Voluptates nobis velit non voluptatem a
+          autem expedita quasi eos iusto ratione, ducimus excepturi voluptas
+          eligendi numquam.
+        </p>
       </div>
     </div>
   );
